751bf4de5169058d5c882aa5c289914b97e61bd0
Kumar <vijayaradhya22@gmail.com>
2023-02-25 21:25:13 +0530

    Initial commit

---
+++ src/main/java/com/aspirants/microservices/limitservice/LimitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aspirants.microservices.limitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitServiceApplication.class, args);
+	}
+
+}
